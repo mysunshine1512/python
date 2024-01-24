ad3fbb7d08ad138b9bb0488f50e5ba119304b906
mysunshine1512 <hoangvobatong1512@gmail.com>
2024-01-24 10:45:00 +0700

    tao file information

---
+++ displayInformation.py
@@ -0,0 +1,1 @@
+print("Họ và tên: Hoàng Võ Bá Tòng \nNgày sinh: 15/12/1993 \nĐịa chỉ: Diên Đại, Phú Xuân, Phú Vang, TT Huế \nMục đích đến với khóa học python: muốn chuyển ngành và tìm kiếm công việc mới")
