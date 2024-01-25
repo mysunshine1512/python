043fc65df64dde181cbb52863e2e49945185ff21
mysunshine1512 <hoangvobatong1512@gmail.com>
2024-01-25 22:06:57 +0700

    ngay trong tuan

---
+++ ngay_trong_tuan.py
@@ -0,0 +1,17 @@
+num_day = int(input("Nhập số ngày trong tuần: "))
+if num_day == 1 :
+    print("Monday")
+elif num_day == 2 :
+    print("Tuesday")
+elif num_day == 3 :
+    print("Wednesday")
+elif num_day == 4 :
+    print("Thursday")
+elif num_day == 5 :
+    print("Friday")
+elif num_day == 6 :
+    print("Saturday")
+elif num_day == 7 :
+    print("Sunday")
+else :
+    print("Nằm ngoài số quy định")
