9984b9e482a35c1845e9d25393a23727f685252d
mysunshine1512 <hoangvobatong1512@gmail.com>
2024-01-25 20:46:24 +0700

    tim so chan le bang if else

---
+++ kiem_tra_chan_le_if_else.py
@@ -0,0 +1,7 @@
+a = float(input("Nhập số cần kiểm tra: "))
+if a % 2 == 0 :
+    print("Số vừa nhập là số chẵn")
+elif a % 2 == 1 :
+    print("Số vừa nhập là số lẻ")
+else :
+    print("Số vừa nhập không phải số tự nhiên")
